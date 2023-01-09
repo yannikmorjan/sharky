9fe45bbccc93ce65b0eb28adffa3bd629e7d8a7f
yannikmorjan <yannik.morjan@web.de>
2023-01-09 16:24:56 +0100

    updated character movement

--- models/character.class.js
+++ models/character.class.js
@@ -1,8 +1,5 @@
 class Character extends MovableObject {
 
-    width = 300;
-    height = 300;
-    speed = 5;
     IMAGES_IDL = [
         'img/1.Sharkie/1.IDLE/1.png',
         'img/1.Sharkie/1.IDLE/2.png',
@@ -23,6 +20,22 @@ class Character extends MovableObject {
         'img/1.Sharkie/1.IDLE/17.png',
         'img/1.Sharkie/1.IDLE/18.png'
     ];
+    IMAGES_IDL_LONG = [
+        'img/1.Sharkie/2.Long_IDLE/1.png',
+        'img/1.Sharkie/2.Long_IDLE/2.png',
+        'img/1.Sharkie/2.Long_IDLE/3.png',
+        'img/1.Sharkie/2.Long_IDLE/4.png',
+        'img/1.Sharkie/2.Long_IDLE/5.png',
+        'img/1.Sharkie/2.Long_IDLE/6.png',
+        'img/1.Sharkie/2.Long_IDLE/7.png',
+        'img/1.Sharkie/2.Long_IDLE/8.png',
+        'img/1.Sharkie/2.Long_IDLE/9.png',
+        'img/1.Sharkie/2.Long_IDLE/10.png',
+        'img/1.Sharkie/2.Long_IDLE/11.png',
+        'img/1.Sharkie/2.Long_IDLE/12.png',
+        'img/1.Sharkie/2.Long_IDLE/13.png',
+        'img/1.Sharkie/2.Long_IDLE/14.png'
+    ];
     IMAGES_SWIM = [
         'img/1.Sharkie/3.Swim/1.png',
         'img/1.Sharkie/3.Swim/2.png',
@@ -30,16 +43,32 @@ class Character extends MovableObject {
         'img/1.Sharkie/3.Swim/4.png',
         'img/1.Sharkie/3.Swim/5.png',
         'img/1.Sharkie/3.Swim/6.png'
-    ]
+    ];
+    IMAGES_FIN_SLAP = [
+        'img/1.Sharkie/4.Attack/Fin slap/1.png',
+        'img/1.Sharkie/4.Attack/Fin slap/2.png',
+        'img/1.Sharkie/4.Attack/Fin slap/3.png',
+        'img/1.Sharkie/4.Attack/Fin slap/4.png',
+        'img/1.Sharkie/4.Attack/Fin slap/5.png',
+        'img/1.Sharkie/4.Attack/Fin slap/6.png',
+        'img/1.Sharkie/4.Attack/Fin slap/7.png',
+        'img/1.Sharkie/4.Attack/Fin slap/8.png'
+    ];
+    width = 300;
+    height = 300;
+    x = 0;
+    y = 0;
+    speed = 0;
+    acceloration = 0.1;
     world;
     currentImage = 0;
 
     constructor() {
         super().loadImage('img/1.Sharkie/1.IDLE/1.png');
         this.loadImages(this.IMAGES_IDL);
+        this.loadImages(this.IMAGES_IDL_LONG);
         this.loadImages(this.IMAGES_SWIM);
-        this.x = 0;
-        this.y = 0;
+        this.loadImages(this.IMAGES_FIN_SLAP);
 
         this.animate();
     }
@@ -49,24 +78,37 @@ class Character extends MovableObject {
             if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.applySwimResistance();
             }
             if(this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.applySwimResistance();
+            }
+            if(this.world.keyboard.UP && this.y > -130) {
+                this.y -= this.speed;
+                this.applySwimResistance();
+            }
+            if(this.world.keyboard.DOWN && this.y < 230) {
+                this.y += this.speed;
+                this.applySwimResistance();
             }
             this.world.camera_x = -this.x;
         },1000 / 60);
 
         setInterval( () => {
-            if((this.world.keyboard.RIGHT || this.world.keyboard.LEFT) && (this.x > 0 && this.x < this.world.level.level_end_x)) {
+            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.DOWN) {
                 this.playAnimation(this.IMAGES_SWIM);
             } else {
                 this.playAnimation(this.IMAGES_IDL);
+                this.speed = 0;
             }
         },250);
     }
 
-    jump() {
-
+    applySwimResistance() {
+        if(this.speed <= 10) {
+            this.speed += this.acceloration;
+        }
     }
 }
