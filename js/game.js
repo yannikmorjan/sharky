13cb981c85dc372448db5f118e677d8eb16df499
yannikmorjan <yannik.morjan@web.de>
2023-02-16 21:32:02 +0100

    added fullscreen

--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
 let canvas;
 let world;
 let keyboard = new Keyboard();
+let sound = false;
+let fullscreen = false;
 
 function init() {
     canvas = document.getElementById('canvas');
@@ -65,14 +67,14 @@ window.addEventListener('keyup', (e) => {
 function openSettings() {
     document.getElementById('top-pannel').innerHTML = ``;
     document.getElementById('mid-pannel').innerHTML = `
-        <div onclick="closeSettings()" class="settingPannel">
+        <div class="settingPannel">
             <img src="" class="">
-            <span>Settings</span>
+            <span onclick="closeSettings()">Settings</span>
             <a>
                 <img>
                 <span>Toggle Sound</span>
             </a>
-            <a>
+            <a id="fullscreen-button">
                 <img>
                 <span>Toggle Fullscreen</span>
             </a>
@@ -88,6 +90,11 @@ function openSettings() {
     document.getElementById('bottom-pannel').innerHTML = `
         <a href="#">Data Privacy</a>
         <a href="#">Imprint</a>`;
+    if(fullscreen) {
+        document.getElementById('fullscreen-button').onclick = function() {closeFullscreen();}
+    } else {
+        document.getElementById('fullscreen-button').onclick = function() {openFullscreen();}
+    }
 }
 
 function closeSettings() {
@@ -95,4 +102,34 @@ function closeSettings() {
     <img onclick="openSettings()" src="img/6.Botones/Settings/settings.png">`;
     document.getElementById('mid-pannel').innerHTML = ``;
     document.getElementById('bottom-pannel').innerHTML = ``;
+}
+
+function openFullscreen() {
+    document.getElementById('canvas-frame').classList.add('d-none');
+    elem = document.getElementById('canvas-container');
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    document.getElementById('canvas').style.width = '100vw';
+    document.getElementById('canvas').style.height = '100vh';
+    fullscreen = true;
+    closeSettings();
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    document.getElementById('canvas').style.width = '720px';
+    document.getElementById('canvas').style.height = '480px';
+    fullscreen = false;
+    closeSettings();
 }
