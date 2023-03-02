ec6aefe9d86cc18d56b8ea0e3f660a486e8ccb7b
yannikmorjan <yannik.morjan@web.de>
2023-03-02 21:17:21 +0100

    updated event handler

--- js/game.js
+++ js/game.js
@@ -21,63 +21,6 @@ function setPausableFn(self, fn) {
     }
 }
 
-window.addEventListener('keydown', (e) => {
-    if(e.keyCode == 39 || e.keyCode == 68) {
-        keyboard.RIGHT = true;
-    }
-    if(e.keyCode == 37 || e.keyCode == 65) {
-        keyboard.LEFT = true;
-    }
-    if(e.keyCode == 38 || e.keyCode == 87) {
-        keyboard.UP = true;
-    }
-    if(e.keyCode == 40 || e.keyCode == 83) {
-        keyboard.DOWN = true;
-    }
-    if(e.keyCode == 72) {
-        keyboard.H = true;
-    }
-    if(e.keyCode == 74) {
-        keyboard.J = true;
-    }
-    if(e.keyCode == 32) {
-        keyboard.SPACE = true;
-    }
-});
-
-window.addEventListener('keyup', (e) => {
-    if(e.keyCode == 39 || e.keyCode == 68) {
-        keyboard.RIGHT = false;
-    }
-    if(e.keyCode == 37 || e.keyCode == 65) {
-        keyboard.LEFT = false;
-    }
-    if(e.keyCode == 38 || e.keyCode == 87) {
-        keyboard.UP = false;
-    }
-    if(e.keyCode == 40 || e.keyCode == 83) {
-        keyboard.DOWN = false;
-    }
-    if(e.keyCode == 72) {
-        keyboard.H = false;
-        world.character.isBubbleAttacking = false;
-    }
-    if(e.keyCode == 74) {
-        keyboard.J = false;
-        world.character.isBubbleAttacking = false;
-    }
-    if(e.keyCode == 32) {
-        keyboard.SPACE = false;
-        world.character.isFinSlaping = false;
-        world.character.invincible = false;
-        world.character.finSlaped = false;
-    }
-});
-document.addEventListener('fullscreenchange', exitFullscreenEventHandler);
-document.addEventListener('webkitfullscreenchange', exitFullscreenEventHandler);
-document.addEventListener('mozfullscreenchange', exitFullscreenEventHandler);
-document.addEventListener('MSFullscreenChange', exitFullscreenEventHandler);
-
 function openSettings() {
     gameIsPaused = true;
     document.getElementById('top-pannel').innerHTML = ``;
